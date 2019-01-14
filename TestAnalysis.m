96029e15632716ac06c3f473e27f9b89f03edb81
Steven Jerjian <steven.jerjian@gmail.com>
2019-01-14 22:13:49 +0000

    Improved ftfreqdata analysis and plotting procedure

--- TestAnalysis.m
+++ TestAnalysis.m
@@ -1,26 +1,20 @@
 
+% change folder to match local
+folder = '/Users/stevenjerjian/Desktop/Senscapes/Meditation Project/Public Dissemination/Dull, Clarity, Open Presence/';
+subj   = 'MED_007';
+files = dir('*.cnt');
+
 close all
-for i=1:2
-    
+for i=1:length(files)
+
     cfg=[];
-    if i==1
-        cfg.dataset='/Users/stevenjerjian/Desktop/Senscapes/Meditation Project/Public Dissemination/Dull, Clarity, Open Presence/MED_007/Run_007_OpenPresence.cnt';
-    else
-        cfg.dataset='/Users/stevenjerjian/Desktop/Senscapes/Meditation Project/Public Dissemination/Dull, Clarity, Open Presence/MED_007/Run_005_Dull.cnt';
-    end
-    
+    cfg.dataset = [files(i).folder '/' files(i).name];
     cfg.bpfilter = 'yes';
     cfg.bsfilter = 'yes';
-    cfg.bsfreq = [49.5 50.5; 99.5 100.5];
-    cfg.bpfreq = [4 120];
-    ftdata=ft_preprocessing(cfg);
-    
-    % figure
-    % pwelch(data.trial{1}(10,:),[],[],[],data.fsample)
-    % hold on
-    % pwelch(data2.trial{1}(10,:),[],[],[],data.fsample)
-    
-    
+    cfg.bsfreq   = [49.5 50.5; 99.5 100.5];
+    cfg.bpfreq   = [4 120];
+    ftdata = ft_preprocessing(cfg);
+
     cfg = [];
     cfg.length = 2;
     ftdata_epoch = ft_redefinetrial(cfg,ftdata);
@@ -32,26 +26,26 @@ for i=1:2
     cfg.tapsmofrq = 3;
     cfg.foi   = [4:128];
     cfg.pad='nextpow2';
-    freqdata = ft_freqanalysis(cfg, ftdata_epoch);
+    freqdata(i) = ft_freqanalysis(cfg, ftdata_epoch);
     
-    
-    figure(1)
-    plot(freqdata.freq,squeeze(mean(freqdata.powspctrm,1)))
-    %
-    %
-    
-    figure(2)
-    banddef = [4 8;9 13;14 30;30 60;60 120]; bandname = {'theta','alpha','beta','low gamma','high gamma'};
+end
+
+%% Plot Sensor Distributions 
+figure;
+banddef  = [4 8;9 13;14 30;30 60;60 120]; 
+bandname = {'theta','alpha','beta','low gamma','high gamma'};
+for i=1:length(files)
+    [~,fname]=fileparts(files(i).name);
+    fname = strsplit(fname,'_');
     for band = 1:5
-        subplot(2,5,band+5*(i-1))
-        title({'Sensor Distribution of ' bandname{band} ' power'})
+        subplot(length(files),5,band+5*(i-1))
+        title(sprintf('%s: Sensor Dist\n %s power',fname{end},bandname{band}))
         cfg        = [];
         cfg.layout = 'biosemi32.lay';
         cfg.xlim   = banddef(band,:);
         cfg.parameter = 'powspctrm';
         % cfg.interactive = 'yes'; %% select this and you can choose the channels!
-        ft_topoplotER(cfg, freqdata);
-        %     savefigure_v2([R.projectfold 'images\sensorspace\'],['sensorDist_' bandname{band}],[],[],[]); close all
+        ft_topoplotER(cfg, freqdata(i));
     end
 end
 
